434dee7562025fd18bf16ecabf0c1a0351835087
Marwys <114246393+Marwys@users.noreply.github.com>
2026-01-28 14:24:13 -0500

    Add files via upload
    Endpoints

---
+++ aws-config.js
@@ -0,0 +1,5 @@
+window.awsConfig = {
+  region: "us-east-1",
+  identityPoolId: "us-east-1:7860e2f2-eb64-4494-9c2a-fe735c8912ec",
+  iotEndpoint: "a1eyoxsww0xvbl-ats.iot.us-east-1.amazonaws.com"
+};
